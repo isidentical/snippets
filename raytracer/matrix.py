52f59cbfcd33761758e1015c2242968446f04a25
Batuhan Taşkaya <btaskaya33@gmail.com>
2019-05-18 21:58:25 +0300

    remove raytracer

--- raytracer/matrix.py
+++
@@ -1,6 +0,0 @@
-from collections import UserList
-
-class Matrix(UserList):
-    def __init__(self, width, length, fill_with = None):
-        filler = fill_with or (lambda: 0,)
-        self.data = [([filler[0](*filler[1:])] * width).copy() for _ in range(length)]
