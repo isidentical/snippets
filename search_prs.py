2824fac0c96181be6470d5c4b2c51d4bcfebb5cf
Batuhan Taşkaya <47358913+isidentical@users.noreply.github.com>
2020-01-01 21:58:25 +0300

    search prs (web)

---
+++ search_prs.py
@@ -0,0 +1,24 @@
+import csv
+from argparse import ArgumentParser
+
+def filter_prs(query):
+    with open("prs.csv") as prs:
+        reader = csv.DictReader(prs)
+        for row in reader:
+            if row["State"] != "open":
+                continue
+            if query in row["Title"].lower():
+                yield row
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("query")
+    namespace = parser.parse_args()
+    pull_requests = filter_prs(namespace.query)
+    
+    for pull_request in pull_requests:
+        print(f"#{pull_request['#']} - {pull_request['Title']} (by {pull_request['User']}) {pull_request['URL']}")
+
+if __name__ == "__main__":
+    main()
+    
